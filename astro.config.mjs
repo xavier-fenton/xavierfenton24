e366f076c35b216a62c47943b4a58e9579de8c71
Xavier Fenton <xavierfenton99@gmail.com>
2024-01-31 14:10:12 +1300

    not sure whats wrong

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,22 @@
-import { sanityIntegration } from '@sanity/astro';
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { sanityIntegration } from '@sanity/astro'
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [sanityIntegration({
-    projectId: 'pucirtyi',
-    dataset: 'production',
-    // Set useCdn to false if you're building statically.
-    useCdn: false,
-    // Access the Studio on your.url/admin
-    studioBasePath: '/admin'
-  }), react()],
-  adapter: vercel()
-});
+  integrations: [
+    sanityIntegration({
+      projectId: 'pucirtyi',
+      dataset: 'production',
+      // Set useCdn to false if you're building statically.
+      useCdn: false,
+      // Access the Studio on your.url/admin
+      studioBasePath: '/admin',
+    }),
+    react(),
+  ],
+  adapter: vercel(),
+})
